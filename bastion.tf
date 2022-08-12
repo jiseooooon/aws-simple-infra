4cc40feb7c618c4d8b1ea0787e99fa7bc6cfad73
jiseon.kim <jiseon.kim@bespinglobal.com>
2022-08-12 19:27:06 +0900

    Initialize

--- bastion.tf
+++ bastion.tf
@@ -1,12 +1,12 @@
-module "bastion-ec2" {
-  source = "./common/terraform-aws-ec2-instance"
-
-  name                        = "${var.resource_prefix}-Bastion"
+resource "aws_instance" "bastion" {
   ami                         = data.aws_ami.amazon-linux2
   instance_type               = var.bastion_instance_type
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.tier-public-subnet-1.id
   key_name                    = local.test_key
   vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
-  monitoring                  = true
+
+  tags = {
+    Name = "${var.resource_prefix}-Bastion"
+  }
 }
